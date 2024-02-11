f9b7d7625b53fe977c5f5cf98354dbc40d6d8d0b
VLAD <buladzislau@gmail.com>
2024-02-11 23:46:34 +0300

    feat: add constants, variables and import base style antd

---
+++ src/constants.ts
@@ -0,0 +1,34 @@
+export const windowSize = {
+    large: 1440,
+    medium: 834,
+    small: 360
+}
+
+export const sizes = {
+    header: 168,
+    sidebar: {
+        mobile: {
+            collapsed: 0,
+            full: 106,
+        },
+        desktop: {
+            collapsed: 64,
+            full: 208,
+        }
+    },
+}
+
+export const colors = {
+    white: {
+        standard: '#FFFFFF',
+    },
+    blue: {
+        light: '#F0F5FF',
+        ocean: '#2F54EB',
+        standard: '#061178'
+    },
+    grey: {
+        light: '#F0F0F0',
+        primary: '#8C8C8C',
+    }
+}
