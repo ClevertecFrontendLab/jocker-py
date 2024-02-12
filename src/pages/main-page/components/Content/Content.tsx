f16f64094bdd77a4665e8592de966e7bca814408
VLAD <buladzislau@gmail.com>
2024-02-12 16:16:20 +0300

    refactor: remove return for empty function

--- src/pages/main-page/components/Content/Content.tsx
+++ src/pages/main-page/components/Content/Content.tsx
@@ -50,81 +50,79 @@ const cards = [
     { id: 2, icon: <IdcardOutlined />, title: 'Заполнить профиль', action: 'Профиль', href: '/' },
 ];
 
-export const Content = () => {
-    return (
-        <Layout.Content id='content'>
-            <ul className='content_list'>
-                {contentDescription.map((item) => (
-                    <li key={item.id} className='content_list__item'>
-                        {item.text}
-                    </li>
-                ))}
-            </ul>
+export const Content = () => (
+    <Layout.Content id='content'>
+        <ul className='content_list'>
+            {contentDescription.map((item) => (
+                <li key={item.id} className='content_list__item'>
+                    {item.text}
+                </li>
+            ))}
+        </ul>
 
-            <div className='content_application'>
-                <Typography.Title level={4} id='content_application__title'>
-                    CleverFit — это не просто приложение, а твой личный помощник в мире фитнеса. Не
-                    откладывай на завтра — начни тренироваться уже сегодня!
-                </Typography.Title>
-                <div className='content_application__list'>
-                    {cards.map(({ id, icon, title, action, href }) => (
-                        <Card
-                            key={id}
-                            actions={[
-                                <Button
-                                    type='link'
-                                    href={href}
-                                    icon={icon}
-                                    style={{
-                                        width: '100%',
-                                        textAlign: 'center',
-                                        padding: '12px',
-                                        color: colors.blue.ocean,
-                                    }}
-                                >
-                                    {action}
-                                </Button>,
-                            ]}
+        <div className='content_application'>
+            <Typography.Title level={4} id='content_application__title'>
+                CleverFit — это не просто приложение, а твой личный помощник в мире фитнеса. Не
+                откладывай на завтра — начни тренироваться уже сегодня!
+            </Typography.Title>
+            <div className='content_application__list'>
+                {cards.map(({ id, icon, title, action, href }) => (
+                    <Card
+                        key={id}
+                        actions={[
+                            <Button
+                                type='link'
+                                href={href}
+                                icon={icon}
+                                style={{
+                                    width: '100%',
+                                    textAlign: 'center',
+                                    padding: '12px',
+                                    color: colors.blue.ocean,
+                                }}
+                            >
+                                {action}
+                            </Button>,
+                        ]}
+                    >
+                        {title}
+                    </Card>
+                ))}
+            </div>
+        </div>
+        <div className='content_additional'>
+            <Button type='link' href={'./'} id='footerReview'>
+                Смотреть отзывы
+            </Button>
+            <Card
+                id='footerCard'
+                bordered={false}
+                actions={[
+                    <>
+                        <Button
+                            id='footerAndroid'
+                            type='text'
+                            href='./'
+                            icon={<AndroidFilled width='14px' height='14px' />}
                         >
-                            {title}
-                        </Card>
-                    ))}
-                </div>
-            </div>
-            <div className='content_additional'>
-                <Button type='link' href={'./'} id="footerReview">
-                    Смотреть отзывы
+                            Android OS
+                        </Button>
+                        <Button
+                            id='footerApple'
+                            type='text'
+                            href='./'
+                            icon={<AppleFilled width='14px' height='14px' />}
+                        >
+                            Apple iOS
+                        </Button>
+                    </>,
+                ]}
+            >
+                <Button type='link' href='./' id='footer__phone'>
+                    Скачать на телефон
                 </Button>
-                <Card
-                    id="footerCard"
-                    bordered={false}
-                    actions={[
-                        <>
-                            <Button
-                                id="footerAndroid"
-                                type='text'
-                                href='./'
-                                icon={<AndroidFilled width='14px' height='14px' />}
-                            >
-                                Android OS
-                            </Button>
-                            <Button
-                                id="footerApple"
-                                type='text'
-                                href='./'
-                                icon={<AppleFilled width='14px' height='14px' />}
-                            >
-                                Apple iOS
-                            </Button>
-                        </>,
-                    ]}
-                >
-                    <Button type='link' href='./' id="footer__phone">
-                        Скачать на телефон
-                    </Button>
-                    <Typography.Text id="footer__pro">Доступно в PRO-тарифе</Typography.Text>
-                </Card>
-            </div>
-        </Layout.Content>
-    );
-};
+                <Typography.Text id='footer__pro'>Доступно в PRO-тарифе</Typography.Text>
+            </Card>
+        </div>
+    </Layout.Content>
+);
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,15 +1,13 @@
 import { Layout } from 'antd';
-import { Header, Content, Sidebar } from './components';
+import { Content, Header, Sidebar } from './components';
 import './style.css';
 
-export const MainPage = () => {
-    return (
-        <Layout hasSider id="mainLayout" style={{minHeight: '100vh'}}>
-            <Sidebar />
-            <Layout>
-                <Header />
-                <Content />
-            </Layout>
+export const MainPage = () => (
+    <Layout hasSider id='mainLayout' style={{ minHeight: '100vh' }}>
+        <Sidebar />
+        <Layout>
+            <Header />
+            <Content />
         </Layout>
-    );
-};
+    </Layout>
+);
